622d6fb3a415e01bb07f20ba41ca0f75a8a7997e
Nilesh Manivannan <nilesh.manivannan@gmail.com>
2023-09-09 21:12:05 -0400

    Initial Commit

---
+++ FirstProgrammingAssignment/FirstProgrammingAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProgrammingAssignmentApp.swift
+//  FirstProgrammingAssignment
+//
+//  Created by Nilesh Manivannan on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstProgrammingAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
