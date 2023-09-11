144dae5bfdd58cad81287e88572014e1625afc55
Nilesh Manivannan <nilesh.manivannan@gmail.com>
2023-09-11 16:21:05 -0400

    Add stock quote search functionality

--- FirstProgrammingAssignment/DashboardView.swift
+++ FirstProgrammingAssignment/DashboardView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 import Firebase
+import FinnhubSwift
 
 struct DashboardView: View {
-    @Binding var loggedIn : Bool
+    @Binding var loggedIn: Bool
+    @State var stockData: Quote?
+    @State var selectedStock: String = ""
+    @State var timestamp: String = ""
     
     var body: some View {
         if loggedIn {
@@ -20,27 +24,68 @@ struct DashboardView: View {
     }
     
     var dashboard : some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Button(action: {
-                do {
-                    try Auth.auth().signOut()
-                    self.loggedIn.toggle()
-                } catch let signOutError as NSError {
-                    print("Error signing out: %@", signOutError)
+        NavigationStack {
+            VStack {
+                Button(action: {
+                    do {
+                        try Auth.auth().signOut()
+                        self.loggedIn.toggle()
+                    } catch let signOutError as NSError {
+                        print("Error signing out: %@", signOutError)
+                    }
+                }, label: {
+                    Text("Log Out")
+                        .frame(width: 200, height:50)
+                        .foregroundColor(Color.white)
+                        .background(Color.black)
+                })
+                .padding()
+                
+                TextField("Enter a Stock Symbol", text: $selectedStock)
+                Button(action: {
+                    getStockData()
+                }, label: {
+                    Text("Get Quote for Stock")
+                        .frame(width: 200, height:50)
+                        .foregroundColor(Color.white)
+                        .background(Color.black)
+                })
+                .padding()
+                
+                if let stockQuote = stockData {
+                    Text("Timestamp : " + timestamp)
+                    Text("Current Price : " + stockQuote.current.description);
+                    Text("Open Price of the Day : " + stockQuote.open.description);
+                    Text("High Price of the Day : " + stockQuote.high.description);
+                    Text("Low Price of the Day : " + stockQuote.low.description);
+                    Text("Previous Close Price : " + stockQuote.previousClose.description);
                 }
-            }, label: {
-                Text("Log Out")
-                    .frame(width: 200, height:50)
-                    .foregroundColor(Color.white)
-                    .background(Color.black)
-            })
-            .padding()
+            }
+        }
+    }
+    
+    private func getStockData() {
+        FinnhubClient.quote(symbol: selectedStock) { result in
+            switch result {
+            case let .success(data):
+                self.stockData = data
+                let date = Date(timeIntervalSince1970: Double(data.timestamp))
+                let dateFormatter = DateFormatter()
+                dateFormatter.timeZone = TimeZone(abbreviation: "EST")
+                dateFormatter.locale = NSLocale.current
+                dateFormatter.dateFormat = "h:mm a 'on' MMMM dd, yyyy"
+                dateFormatter.amSymbol = "AM"
+                dateFormatter.pmSymbol = "PM"
+                self.timestamp = dateFormatter.string(from: date)
+            case .failure(.invalidData):
+                print("Invalid data")
+            case let .failure(.networkFailure(error)):
+                print(error)
+            }
         }
     }
 }
 
-//
 //struct DashboardView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        DashboardView()
